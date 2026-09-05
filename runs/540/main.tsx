f89732711f585b3cd7a6bb1879b4ba60fd9dfeaf
Gimtrist Run Builder <run-builder@gimtrist.invalid>
2026-09-05 15:33:21 +0000

    Publish Gimtrist Run target 540 build 185

---
+++ runs/540/main.tsx
@@ -0,0 +1,101 @@
+import { Game } from 'phaser';
+
+import {
+  Fragment,
+  Rectangle,
+  render,
+  Text,
+  useEffect,
+  useScene,
+  useState,
+} from '../../src';
+
+function Clicker() {
+  const [count, setCount] = useState(0);
+  const [hovered, setHovered] = useState(false);
+
+  useEffect(() => {
+    console.log('count:', count);
+  }, [count]);
+
+  return (
+    <Fragment>
+      {undefined}
+      {null}
+      {false}
+      {0}
+
+      {count > 0 ? (
+        <Text text={`Clicks: ${count}`} x={16} y={16} />
+      ) : (
+        <Text text="You clicked 0 times" x={16} y={16} />
+      )}
+
+      <Text
+        text="Click"
+        x={16}
+        y={40}
+        depth={1}
+        style={{
+          backgroundColor: hovered ? '#87ceeb' : '#fff',
+          color: '#000',
+          padding: { x: 12 + count, y: 8 + count },
+        }}
+        input={{ cursor: 'pointer' }}
+        onPointerOver={() => {
+          setHovered(true);
+        }}
+        onPointerOut={() => {
+          setHovered(false);
+        }}
+        onPointerDown={() => {
+          setCount(count + 1);
+        }}
+      />
+
+      <Rectangle
+        x={16}
+        y={80}
+        width={120}
+        height={60}
+        fillColor={0x6495ed}
+        originX={0}
+        originY={0}
+      />
+    </Fragment>
+  );
+}
+
+function Time() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const scene = useScene();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Text
+      text={time}
+      x={scene.scale.width - 16}
+      y={16}
+      style={{ color: '#fff' }}
+      originX={1}
+      originY={0}
+    />
+  );
+}
+
+new Game({
+  width: 800,
+  height: 600,
+  scene: {
+    create() {
+      render(<Clicker />, this);
+      render(<Time />, this);
+    },
+  },
+});
